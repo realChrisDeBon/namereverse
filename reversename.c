4149f11e8b85ab294517a9b69d568325f35f55d4
Chris DeBon <97779307+realChrisDeBon@users.noreply.github.com>
2023-08-31 11:00:39 -0500

    Add files via upload

---
+++ reversename.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <wchar.h>
+#include <windows.h>
+#include <locale.h>
+#include <string.h>
+
+int main()
+{
+    setlocale(LC_CTYPE, ".utf8");
+    wchar_t reverse_char = 0x202E; // Reverse character
+    wchar_t target_name[20], new_name[30];
+
+    printf("File to be renamed: ");
+    wscanf(L"%19ls", &target_name);
+
+    printf("\nUse character ~ to initiate reveral of characters.\n");
+    printf("New filename: ");
+    wscanf(L"%29ls", &new_name);
+
+    for (int i = 0; i < wcslen(new_name); i++) {
+        if (new_name[i] == L'~') {
+            new_name[i] = reverse_char;
+        }
+    }
+
+    int attempt_rename = _wrename(target_name, new_name);
+    if (attempt_rename == 0)
+    {
+        printf("Rename success.\n");
+    } else {
+        printf("Rename failed.\n");
+    }
+    system("pause");
+    return 0;
+}
